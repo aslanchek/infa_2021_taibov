3b5ded42720777bc4f15847ad83d4720c9220fbf
aslanchek <taibov.0321@mail.ru>
2021-10-21 14:48:12 +0300

    random collisions

--- Ball.py
+++ Ball.py
@@ -6,11 +6,11 @@ class Ball():
     def __init__(self, game):
         self.game = game
 
-        self.r = randint(10, 100)
+        self.r = randint(20, 50)
         self.x = randint(self.r, SCREEN_WIDTH-self.r)
         self.y = randint(self.r, SCREEN_HEIGHT-self.r)
-        self.Vx = randint(1, 10)
-        self.Vy = randint(1, 10)
+        self.Vx = randint(-10, 10)
+        self.Vy = randint(-10, 10)
         self.color = (randint(0, 255), randint(0, 255), randint(0, 255))
 
     def render(self, screen):
@@ -20,11 +20,21 @@ class Ball():
         self.x += self.Vx
         self.y += self.Vy
 
-        if self.x + self.r > SCREEN_WIDTH or self.x - self.r < 0: 
-            self.Vx *= -1
+        if self.x + self.r > SCREEN_WIDTH:
+            self.Vx = randint(-10, 0)
+            self.Vy = randint(-10, 10)
 
-        if self.y + self.r > SCREEN_HEIGHT or self.y - self.r < 0: 
-            self.Vy *= -1
+        if self.x - self.r < 0: 
+            self.Vx = randint(0, 10)
+            self.Vy = randint(-10, 10)
+
+
+        if self.y + self.r > SCREEN_HEIGHT:
+            self.Vx = randint(-10, 10)
+            self.Vy = randint(-10, 0)
+        if self.y - self.r < 0: 
+            self.Vx = randint(-10, -10)
+            self.Vy = randint(0, 10)
 
     def remove(self):
         self.game.objects.remove(self)
